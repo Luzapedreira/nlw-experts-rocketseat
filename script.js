b158fce1a3f99b0f31e9b57e4cfc9ccc47c3f2aa
Luza <90648447+Luzapedreira@users.noreply.github.com>
2024-02-08 10:31:39 -0300

    tema final

--- script.js
+++ script.js
@@ -90,74 +90,43 @@ const perguntas = [
     correta: 1
   }
 ];
-  
-  const quiz = document.querySelector('#quiz')
-  const template = document.querySelector('template')
-  
-  const corretconst perguntas = [
-    {
-      pergunta: "Qual é a finalidade do comando 'console.log()' em JavaScript?",
-      respostas: [
-        "Exibir uma mensagem de erro",
-        "Imprimir dados no console",
-        "Criar uma variável"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual é a função do operador '===' em comparações em JavaScript?",
-      respostas: [
-        "Comparação de valores sem considerar o tipo",
-        "Atribuição de valores",
-        "Comparação estrita de valores e…pt?",
-      respostas: [
-        "São iguais, usados de forma intercambiável",
-        "'null' representa a ausência de valor, 'undefined' é atribuído explicitamente",
-        "Ambos representam valores vazios"
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Como se adiciona um evento a um elemento HTML usando JavaScript?",
-      respostas: [
-        "Apenas com CSS",
-        "Usando o atributo 'event'",
-        "Através do método 'addEventListener'"
-      ],
-      correta: 2
-    },
-  ];as = new Set()
-  const totalDePerguntas = perguntas.length
-  const mostrarTotal = document.querySelector('#acertos span')
-  mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
-  
-  // loop ou laço de repetição
-  for (const item of perguntas) {
-    const quizItem = template.content.cloneNode(true)
-    quizItem.querySelector('h3').textContent = item.pergunta
-  
-    for (let resposta of item.respostas) {
-      const dt = quizItem.querySelector('dl dt').cloneNode(true)
-      dt.querySelector('span').textContent = resposta
-      dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
-      dt.querySelector('input').value = item.respostas.indexOf(resposta)
-      dt.querySelector('input').onchange = (event) => {
-        const estaCorreta = event.target.value == item.correta
-  
-        corretas.delete(item)
-        if (estaCorreta) {
-          corretas.add(item)
-        }
-  
-        mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+
+
+const quiz = document.querySelector('#quiz')
+const template = document.querySelector('template')
+
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+
+// loop ou laço de repetição
+for (const item of perguntas) {
+  const quizItem = template.content.cloneNode(true)
+  quizItem.querySelector('h3').textContent = item.pergunta
+
+  for (let resposta of item.respostas) {
+    const dt = quizItem.querySelector('dl dt').cloneNode(true)
+    dt.querySelector('span').textContent = resposta
+    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+    dt.querySelector('input').value = item.respostas.indexOf(resposta)
+    dt.querySelector('input').onchange = (event) => {
+      const estaCorreta = event.target.value == item.correta
+
+      corretas.delete(item)
+      if (estaCorreta) {
+        corretas.add(item)
       }
-      quizItem.querySelector('dl').appendChild(dt)
+
+      mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
     }
-  
-  
-    quizItem.querySelector('dl dt').remove()
-  
-  
-    // coloca a pergunta na tela
-    quiz.appendChild(quizItem)
+    quizItem.querySelector('dl').appendChild(dt)
   }
+
+
+  quizItem.querySelector('dl dt').remove()
+
+
+  // coloca a pergunta na tela
+  quiz.appendChild(quizItem)
+}
